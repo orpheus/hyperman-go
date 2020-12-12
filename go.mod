a643edadb4909eaa41e0190deac8d8b4ea81e290
ohryan <ryanachacon@gmail.com>
2020-12-12 12:10:24 -0700

    add viper

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/orpheus/hyperman-go
 
 go 1.15
+
+require github.com/spf13/viper v1.7.1
--- main.go
+++ main.go
@@ -1,6 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+func init() {
+	viper.SetConfigName("hyperman")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	if err := viper.ReadInConfig(); err != nil {
+		panic(fmt.Errorf("Fata error config file: %s \n", err))
+	}
+}
 
 func main() {
 	fmt.Println("I am Hyperman")
